11c518073ba9d3317a0686de71e172949b3d2109
Yoshikazu Kobayashi <yoshikazu1koba@gmail.com>
2024-09-29 07:13:09 +0900

    Add VS Code settings for TypeScript formatting and code actions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,42 @@
-const main = document.getElementById('main');
+import Phaser from "phaser";
 
-if (main) {
-    main.innerHTML = 'Hello World!';
+class Example extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("https://labs.phaser.io");
+
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+
+    const particles = this.add.particles(0, 0, "red", {
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: "ADD",
+    });
+
+    const logo = this.physics.add.image(400, 100, "logo");
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    particles.startFollow(logo);
+  }
 }
+
+export const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 480,
+  height: 800,
+  scene: Example,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 600 },
+    },
+  },
+});
